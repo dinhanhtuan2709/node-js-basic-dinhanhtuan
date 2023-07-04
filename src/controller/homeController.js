ec88e12abcc8198f20c8d4a95a2bd1f0d40e488b
dinhanhtuan2709 <dinhanhtuan2709@gmail.com>
2023-07-04 16:48:36 +0700

    REQUEST METHOD & SỬ DỤNG DATABASE VỚI NODE.JS

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,23 @@
+import connection from "../configs/connectDB";
+
 let getHomePage = (req, res) =>  {
-    return res.render('test/index.ejs')
+
+    let data = [];
+    connection.query(
+    'SELECT * FROM `user`',
+    function(err, results, fields) {
+        results.map((row) =>{
+            data.push({
+                 Code : row.Code,
+                Name : row.Name,
+                Email : row.Email,
+                PhoneNumber : row.PhoneNumber,
+                Address : row.Address
+            })
+           
+        });
+            return res.render('test/index.ejs', {dataUser : JSON.stringify(data)})
+    });
 }
 
 module.exports = {
